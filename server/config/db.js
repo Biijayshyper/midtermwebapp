94ea96fb1bca79a51b4e9400d519cf0162252707
Bryan James Mendoza <bryanjamesmendoza1234@gmail.com>
2021-03-07 21:42:57 +0800

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/book_store"
+  "URI":"mongodb+srv://bryan2:91UmKWUxWxznFiMv@mongodbservermidterm.px2ll.mongodb.net/books_store?retryWrites=true&w=majority"
+};
